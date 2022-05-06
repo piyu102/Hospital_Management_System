6c7ad290865c5502e9540b4f4a012ae5f74bec4d
Piyush Agrawal <piyushagrawal102@gmail.com>
2022-05-06 18:42:20 +0530

    Add Frontend

---
+++ HMS_Frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <div style={{ width: "100%" }} className="appJS"> 
+    </div>
+  );
+}
+
+export default App;
+
